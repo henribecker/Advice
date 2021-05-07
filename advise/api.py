69ab1b207cc4b76a7946f5870eda71c9b0e91da2
henribecker <63249159+henribecker@users.noreply.github.com>
2021-05-06 23:09:27 -0300

    b

--- advise/api.py
+++ advise/api.py
@@ -3,6 +3,7 @@ import requests, json
 def get():
     obj = requests.get('https://api.adviceslip.com/advice')
     mjson = json.loads(obj.text)
+    print(obj.status_code)
     for n in mjson.values():
         num = str(n['advice'])
         return num
--- startup/views.py
+++ startup/views.py
@@ -1,13 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
-from django.shortcuts import redirect
-
-
+from django.http import Http404
 
 def index(request):
-
     context = {
         "first": 'Welcome to my advice'
     }
 
     return render(request, 'base.html', context)
+
