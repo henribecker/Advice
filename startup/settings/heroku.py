af145b599b0c2863fa66d3c287a9b9e4634c936b
henribecker <63249159+henribecker@users.noreply.github.com>
2021-05-25 19:27:31 -0300

    heroku

--- startup/settings/heroku.py
+++ startup/settings/heroku.py
@@ -7,7 +7,7 @@ DEBUG = env.bool('DEBUG', False)
 
 SECRET_KEY = env("SECRET_KEY")
 
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=['https://aqueous-thicket-41204.herokuapp.com/',])
 
 DATABASES = {
     "default": env.db(),
