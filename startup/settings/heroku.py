9b46561c9ac3c2724dd2c2e006988dbbc4eb6da6
henribecker <63249159+henribecker@users.noreply.github.com>
2021-05-25 00:26:04 -0300

    ...

--- startup/settings/heroku.py
+++ startup/settings/heroku.py
@@ -7,7 +7,7 @@ DEBUG = env.bool('DEBUG', False)
 
 SECRET_KEY = env("SECRET_KEY")
 
-ALLOWED_HOSTS = env.list("ALLOWED_HOTS")
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
 DATABASES = {
     "default": env.db(),
